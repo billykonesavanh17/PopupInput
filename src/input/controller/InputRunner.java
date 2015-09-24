dfc456508cc46c7874dc7177ab49bbffdef4fe45
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-09-24 08:27:34 -0600

    Creating new project with popups.

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,13 @@
+package input.controller;
+
+public class InputRunner
+{
+	public static void main (String [] args)
+	{
+		//Thing myThing = new Thing();
+		//myThing.Start();
+		PopupController myInput = new PopupController();
+		myInput.start();
+		
+	}
+}
